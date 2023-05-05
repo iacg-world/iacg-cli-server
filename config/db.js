9107585bec1fe45d5cd89253ac609fcf02d8a613
supercll <1033581609@qq.com>
2023-05-05 18:09:22 +0800

    feat: init egg and mongodb

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/** MONGODB **/
+const mongodbUrl = 'mongodb://lc:103358@localhost:27017/iacg-cli'
+const mongodbDbName = 'iacg-cli'
+
+module.exports = {
+  mongodbUrl,
+  mongodbDbName,
+}
